7d1e3cca8ebdad313ca491836c479d38306ed994
Bruno Henrique Gusmão Vasconcelos <brunohgv2@gmail.com>
2017-10-19 02:47:32 -0300

    componentes header e front prontos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FrontComponent } from './front/front.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FrontComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/front/front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.css']
+})
+export class FrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
